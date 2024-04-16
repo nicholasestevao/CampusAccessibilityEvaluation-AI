50c2b033d39d8902024129a50f3c5c06f0093381
Nicholas Estevão <nicholasestevao@Nicholass-MacBook-Air.local>
2024-04-16 12:37:22 -0300

    mapa area 1 sem acessibilidade

---
+++ Trab1/mapaSemAcessibilidade.pl
@@ -0,0 +1,15 @@
+pode_ir(portArq, onibus, 107.37).
+pode_ir(onibus, cefer, 323.13).
+pode_ir(portArq, restaurante, 707.15).
+pode_ir(cefer, restaurante, 178.46).
+pode_ir(restaurante, aquario, 116.78).
+pode_ir(restaurante, biblioEESC, 135.96).
+pode_ir(aquario, biblioEESC, 11.79).
+pode_ir(aquario, icmc5, 81.68).
+pode_ir(biblioEESC, icmc5, 76.11).
+pode_ir(icmc5, icmc1, 139.86).
+pode_ir(icmc5, icmc6, 101.3).
+pode_ir(icmc1, icmc4, 116.59).
+pode_ir(icmc1, icmc3, 93.84).
+pode_ir(icmc3, biblioICMC, 53.17).
+pode_ir(icmc1, biblioICMC, 42.20).
