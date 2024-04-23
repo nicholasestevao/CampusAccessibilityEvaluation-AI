9dd348c393c8c452ddd0c9e4562117da3d5b64f4
ikuyorih9 <hugohiroyuki9@gmail.com>
2024-04-23 17:49:35 -0300

    Mapas com e sem acessibilidade

--- Trab1/mapaSemAcessibilidade.pl
+++ Trab1/mapaSemAcessibilidade.pl
@@ -1,28 +1,29 @@
 % Mapeamento da Area 1 incluindo trechos sem acessibilidade
 
 %pode_ir(? Origem, ? Destino, - Custo)
-pode_ir(onibus, saidaArq, 107.37).
-pode_ir(onibus, cefer, 323.13).
-pode_ir(cefer, restaurante, 178.46).
-pode_ir(restaurante, biblioEESC, 137.00). 
-pode_ir(biblioEESC, biblioICMC, 165.07). 
-pode_ir(matematica, biblioICMC, 196.85).
-pode_ir(matematica, restaurante, 260.91).
-pode_ir(matematica, biblioEESC, 169.87).
-pode_ir(saidaFisica, ifsc, 422.90). %medida da saida ate o predio administrativo do ifsc passando pela escada do E1
-pode_ir(ifsc, e1, 105.37).
-pode_ir(e1, restaurante, 196.54).
-pode_ir(e1, biblioEESC, 205.57).
-pode_ir(e1, biblioICMC, 253.22).
-pode_ir(e1, campo, 306.05). %medida ate o topo da escada para descer pro campo
-pode_ir(saidaProd, ifsc, 345.26).
-pode_ir(iqsc, cefer, 216.67). %medida passando pelo centrinho
-pode_ir(iqsc, campo, 288.27). %medida ate o topo da escada para descer pro campo
-pode_ir(campo, restaurante, 119.81).
-pode_ir(iqsc, saidaArq, 305.76).
-pode_ir(sel, iqsc, 255.64).
-pode_ir(sel, campo, 95.78).
-pode_ir(sel, restaurante, 124.68). %passando pelo degrau do lado do bandeco
+pode_ir(saidaArq, onibus, 107.37).
+    pode_ir(onibus, cefer, 323.13).
+        pode_ir(cefer, degrauRestaurante, 100.92).
+            pode_ir(degrauRestaurante, restaurante, 45.55).
+        pode_ir(cefer, sel, 71.4).
+        pode_ir(cefer, campoEscada1, 144.01).
 
+pode_ir(saidaArq, iqsc, 305.76).
+    pode_ir(iqsc, cefer, 216.67). %medida passando pelo centrinho
+    pode_ir(iqsc, campoEscada1, 89.52).
+        pode_ir(campoEscada1, campoEscada2, 225.29). %Por dentro do campo
+            pode_ir(campoEscada2, degrauRestaurante, 80.65).
+            pode_ir(campoEscada2, escadaSel, 94.78).
+                pode_ir(escadaSel, sel, 21.2).
+                pode_ir(escadaSel, restaurante, 168.98).
+            pode_ir(campoEscada2, restaurante, 134.53).
+
+pode_ir(matematica, biblioICMC, 196.85).
+pode_ir(matematica, biblioEESC, 149.93).
+pode_ir(matematica, restaurante, 260.91).
+    pode_ir(restaurante, biblioEESC, 137.0).
+        pode_ir(biblioEESC, biblioICMC, 294.95).
+        pode_ir(biblioEESC, escadaBloco5, 54.12).
+            pode_ir(escadaBloco5, biblioICMC, 114.92).
 
 podeIr(X,Z, C) :- pode_ir(X,Z, C) | pode_ir(Z,X, C).
